2788d0b9828ceef31bef44b3376f1a2049999b33
LOLITA0164 <476512340@qq.com>
2019-12-11 17:14:33 +0800

    Initial commit

---
+++ Example/LLToolbox/LLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LLAppDelegate.h
+//  LLToolbox
+//
+//  Created by LOLITA0164 on 12/11/2019.
+//  Copyright (c) 2019 LOLITA0164. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/LLToolbox/LLViewController.h
@@ -0,0 +1,13 @@
+//
+//  LLViewController.h
+//  LLToolbox
+//
+//  Created by LOLITA0164 on 12/11/2019.
+//  Copyright (c) 2019 LOLITA0164. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LLViewController : UIViewController
+
+@end
